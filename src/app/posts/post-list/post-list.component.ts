8748a746779a9c08b4dfc26454411bcb9537bc6a
murtaza98 <m.patrawala@somaiya.edu>
2019-12-14 22:55:26 +0530

    Add Observables to the service

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
 
   posts: Post[] = [];
   postsService: PostsService;
+  private postsSubs: Subscription;
 
   constructor(postsService: PostsService) {
     this.postsService = postsService;
@@ -18,5 +20,14 @@ export class PostListComponent implements OnInit {
 
   ngOnInit() {
     this.posts = this.postsService.getPosts();
+    this.postsSubs = this.postsService.getPostUpdateListener().subscribe(
+      (posts) => {
+        this.posts = posts;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.postsSubs.unsubscribe();
   }
 }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,9 +1,11 @@
 import { Post } from './post.model'
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
+  private postUpdated = new Subject<Post[]>();
 
   getPosts() {
     return [...this.posts];     // spread operator for clonning
@@ -11,5 +13,10 @@ export class PostsService {
 
   addPosts(post: Post) {
     this.posts.push(post);
+    this.postUpdated.next([...this.posts]);
+  }
+
+  getPostUpdateListener(){
+    return this.postUpdated.asObservable();
   }
 }
