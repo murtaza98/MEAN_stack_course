04e562666e9a8a2b0ed214ff037910a988ee313d
murtaza98 <m.patrawala@somaiya.edu>
2019-12-16 13:56:57 +0530

    2.5 -- add express to app

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("First MiddleWare");
+  next();   // pass request to next middleWare
+});
 
+app.use((req, res, next) => {
+  res.send("Hello from express");
+});
+
+
+// export this app
+module.exports = app;
 
 
 
